175574fcd8a094d0b475ae984418b908c3d4e86d
Roberto Talamas <robertotalamas@Robertos-MacBook-Pro.local>
2023-04-18 14:26:10 -0500

    initial commit for syncracyGPT

---
+++ syncracyGPT.py
@@ -0,0 +1,185 @@
+import glob
+import random
+
+import streamlit as st
+from PIL import Image
+from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import GitbookLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import OnlinePDFLoader
+from langchain.document_loaders import WebBaseLoader
+
+from utils import get_text, document_pipeline, add_loader_to_session_state, do_nothing
+from utils import new_chat
+
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+# Set Streamlit page configuration
+st.set_page_config(
+    page_title="SyncracyGPT",
+    page_icon="🤖",
+    layout="wide",
+)
+
+markdown = """<h1 style='font-family: Calibri; text-align: center;'><img 
+src="https://images.squarespace-cdn.com/content/v1/63857484f91d71181b02f971/9943adcc-5e69-489f-b4a8-158f20fe0619
+/Snycracy_WebLogo.png?format=500w" alt="logo"/>GPT</h1>"""
+
+st.markdown(markdown, unsafe_allow_html=True)
+
+# ------------------------ Set Initial Session State ------------------------ #
+
+# Initialize session states
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+if "input" not in st.session_state:
+    st.session_state["input"] = ""
+if "stored_session" not in st.session_state:
+    st.session_state["stored_session"] = []
+if "loaders" not in st.session_state:
+    st.session_state["loaders"] = []
+if "training_model_status" not in st.session_state:
+    st.session_state["training_model_status"] = False
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+if "document_db" not in st.session_state:
+    st.session_state["document_db"] = False
+if "load_button_clicked" not in st.session_state:
+    st.session_state["load_button_clicked"] = False
+
+# ------------------------ Set up sidebar with various options ------------------------ #
+
+# Set up sidebar with various options
+with st.sidebar.expander("📈💻 Research Documents", expanded=False):
+
+    # Todo - Add a button to load a youtube video
+    loader_map = {'Gitbook': GitbookLoader,
+                  'URL': WebBaseLoader}
+    document_type = st.selectbox(label='Document Type', options=loader_map.keys(),
+                                 on_change=do_nothing)
+
+    if document_type == 'Gitbook':
+        gitbook_url = st.text_input('Enter Gitbook URL', on_change=do_nothing)
+
+    elif document_type == 'URL':
+        urls_input = st.text_input("Enter URLs", on_change=do_nothing)
+
+    elif document_type == 'PDF':
+        pdf_file = st.text_input('Enter PDF URL', on_change=do_nothing)
+
+    document_description = st.text_area('Enter Document Description', on_change=do_nothing)
+
+    load_docs, train_model = st.columns(2)
+
+    with load_docs:
+        loaded_doc = st.button('Load Documents')
+        if loaded_doc:
+            if document_type == 'Gitbook' and gitbook_url:
+                st.session_state["load_button_clicked"] = True
+            elif document_type == 'URL' and urls_input:
+                st.session_state["load_button_clicked"] = True
+            elif document_type == 'PDF' and pdf_file:
+                st.session_state["load_button_clicked"] = True
+            else:
+                st.warning('Please provide the necessary input (URL or file) for the selected document type')
+
+        if st.session_state["load_button_clicked"]:
+            try:
+                document_loader = loader_map[document_type]
+                if document_type == 'Gitbook' and gitbook_url:
+                    document_loader = GitbookLoader(gitbook_url, load_all_paths=True)
+                elif document_type == 'URL' and urls_input:
+                    document_loader = WebBaseLoader(urls_input)
+                elif document_type == 'PDF' and pdf_file:
+                    document_loader = PyPDFLoader(pdf_file)
+
+                add_loader_to_session_state(document_loader)
+                st.session_state["load_button_clicked"] = False  # Reset the flag
+            except NameError:
+                st.error('Please select a document type and enter the corresponding URL/paths')
+
+    if len(st.session_state["loaders"]) < 1:
+        st.warning('Please load at least one document')
+    else:
+        st.success(f'Successfully loaded {len(st.session_state["loaders"])} documents')
+
+    with train_model:
+        if st.button('Train SyncracyGPT'):
+            with st.spinner('Training SyncracyGPT....'):
+                document_db = document_pipeline()
+                st.session_state['document_db'] = document_db
+                st.session_state['training_model_status'] = True
+
+with st.sidebar.expander("🛠️ Model Settings", expanded=False):
+    MODEL = st.selectbox(label='Model',
+                         options=['gpt-3.5-turbo', 'text-davinci-003',
+                                  'text-davinci-002', 'code-davinci-002'])
+
+# Add a button to start a new chat
+st.sidebar.button("New Chat", on_click=new_chat, type='primary')
+
+# ------------------------ User interface ------------------------ #
+
+# Get the user input
+user_input = get_text()
+
+# Generate the output using the ConversationChain object and the user input, and add the input/output to the session
+if st.session_state['training_model_status']:
+    st.success('SyncracyGPT is ready to chat!')
+
+if user_input:
+    # Create an OpenAI instance
+    llm = ChatOpenAI(temperature=0,
+                     openai_api_key=OPENAI_API_KEY,
+                     callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+                     verbose=True,
+                     model_name=MODEL)
+
+    # Create the ConversationChain object with the specified configuration
+    try:
+        chain = ConversationalRetrievalChain.from_llm(llm=llm,
+                                                      retriever=st.session_state.document_db.as_retriever(),
+                                                      return_source_documents=True)
+    except NameError:
+        st.error('Please train SyncracyGPT before chatting')
+
+    if user_input == "Dan's office robot greetings":
+        images = []
+        for img in glob.glob("dangreetings/*.jpeg"):
+            images.append(Image.open(img))
+        st.image(random.choice(images), caption="Dan's office robot greetings")
+        st.stop()
+    output = chain({"question": user_input, "chat_history": st.session_state.chat_history})
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output['answer'])
+    st.session_state.chat_history.append((user_input, output['answer']))
+
+# Display the conversation history using an expander, and allow the user to download it
+download_str = []
+with st.expander("Conversation", expanded=True):
+    for i in range(len(st.session_state['generated']) - 1, -1, -1):
+        st.info(st.session_state["past"][i])
+        st.success(st.session_state["generated"][i])
+        download_str.append(st.session_state["past"][i])
+        download_str.append(st.session_state["generated"][i])
+
+    # Can throw error - requires fix
+    download_str = '\n'.join(download_str)
+    if download_str:
+        st.download_button('Download', download_str)
+
+# Display stored conversation sessions in the sidebar
+for i, sublist in enumerate(st.session_state.stored_session):
+    with st.sidebar.expander(label=f"Conversation-Session:{i}"):
+        st.write(sublist)
+
+# Allow the user to clear all stored conversation sessions
+if st.session_state.stored_session:
+    if st.sidebar.checkbox("Clear-all"):
+        del st.session_state.stored_session
---
+++ utils.py
@@ -0,0 +1,100 @@
+import streamlit as st
+from langchain.document_loaders import GitbookLoader, WebBaseLoader, PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import OnlinePDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+
+def get_text():
+    """
+    Get the user input text.
+    Returns:
+        (str): The text entered by the user
+    """
+    input_text = st.text_input("You: ", st.session_state["input"], key="input",
+                               placeholder="Type crypto query or 'Dan's office robot greetings' for a robot hello moment",
+                               label_visibility='hidden')
+    return input_text
+
+
+# Define function to start a new chat
+def new_chat():
+    """
+    Clears session state and starts a new chat.
+    """
+    save = []
+    for i in range(len(st.session_state['generated']) - 1, -1, -1):
+        save.append("User:" + st.session_state["past"][i])
+        save.append("Bot:" + st.session_state["generated"][i])
+    st.session_state["stored_session"].append(save)
+    st.session_state["generated"] = []
+    st.session_state["past"] = []
+    st.session_state["input"] = ""
+    st.session_state.entity_memory.store = {}
+    st.session_state.entity_memory.buffer.clear()
+
+
+def do_nothing():
+    """
+    Callback to do nothing when st widgets change
+    """
+    pass
+
+
+def add_loader_to_session_state(loader):
+    """
+    Adds a document loader to the session state.
+    """
+    st.session_state["loaders"].append(loader)
+
+
+def document_pipeline():
+    """
+    Loads documents and pre-processes them for user query
+    """
+    all_documents = []
+    # Add conditionals for different loaders
+    for loader in set(st.session_state["loaders"]):
+        if isinstance(loader, GitbookLoader) or isinstance(loader, WebBaseLoader):
+            documents = loader.load()  # Call the .load() method for GitbookLoader and WebBaseLoader
+            all_documents.extend(documents)
+        elif isinstance(loader, OnlinePDFLoader):
+            documents = loader.load()  # Call the .load_and_split() method for PyPDFLoader
+            all_documents.extend(documents)
+
+    # Split the documents into chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(all_documents)
+
+    # Generate embeddings for each documents
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+    # Add embeddings to Chroma's vector store
+    docsearch = Chroma.from_documents(texts, embeddings)
+
+    return docsearch
+
+
+def create_vectorstore(loader):
+    """
+    Loads documents and pre-processes them for user query
+    """
+    if isinstance(loader, GitbookLoader) or isinstance(loader, WebBaseLoader):
+        documents = loader.load()  # Call the .load() method for GitbookLoader and WebBaseLoader
+    elif isinstance(loader, OnlinePDFLoader):
+        documents = loader.load()
+
+    # Split the documents into chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents=documents)
+
+    # Generate embeddings for each documents
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+    # Add embeddings to Chroma's vector store
+    docsearch = Chroma.from_documents(texts, embeddings)
+
+    return docsearch
